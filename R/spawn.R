928c75be61ec0a1b89c4446b2303a44415db3044
evanchildress <evan.childress@gmail.com>
2016-08-01 16:59:15 -0400

    working on distributing eggs from connected rivers among them equitably based on stream length, but not done

--- R/spawn.R
+++ R/spawn.R
@@ -2,8 +2,19 @@
 #' @param forkLength Vector of fork lengths to estimate fecundity for
 #' @export
 
-spawn<-function(forkLength){
+spawn<-function(forkLength,river){
+  streamLength<-c(47,17,14)
+  streamLength<-streamLength/sum(streamLength)
+
   totalEggs<-0.00187*forkLength^2.19
-  femaleEggs<-round(totalEggs/2)
-  return(femaleEggs)
+  femaleEggs<-totalEggs/2
+
+  eggs<-data.table(river=river,femaleEggs=femaleEggs) %>%
+                bind_rows(data.table(river=1:4,femaleEggs=rep(0,4))) %>%
+                .[,river2:=river] %>%
+                .[river2 %in% 1:3,river2:=1]
+  eggs<-eggs[,.(femaleEggs=sum(femaleEggs)),by=river2]
+  eggsOut<-c(eggs[river2==1,femaleEggs]*streamLength,eggs[river2==4,femaleEggs])
+
+  return(eggsOut)
 }
